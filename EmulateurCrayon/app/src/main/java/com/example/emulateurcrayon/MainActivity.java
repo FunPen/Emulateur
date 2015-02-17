0e23a9e6149a1445fe7618ca2e36dc7db8ed63a2
val66 <valentin.pinilla@epitech.eu>
2015-02-17 13:46:07 +0100

    Reste plus que le protocole à implémenter
    Tout est dans le titre

--- EmulateurCrayon/app/src/main/java/com/example/emulateurcrayon/MainActivity.java
+++ EmulateurCrayon/app/src/main/java/com/example/emulateurcrayon/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.emulateurcrayon;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.UUID;
 
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.bluetooth.BluetoothAdapter;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -32,9 +33,9 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 	private float last_x, last_y, last_z;
 	private static final int SHAKE_THRESHOLD = 100;
 	private static final int REQUEST_ENABLE_BT = 1;
-	
-	private BroadcastReceiver mReceiver;
-	private List<String> mArrayAdapter = new ArrayList<String>();
+    private static final int MESSAGE_READ = 1;
+    private BluetoothSocket mySocket;
+    private ConnectedThread connexion;
 
     private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -73,34 +74,6 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}
-		
-		/*Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-		// If there are paired devices
-		if (pairedDevices.size() > 0) {
-		    // Loop through paired devices
-		    for (BluetoothDevice device : pairedDevices) {
-				// Add the name and address to an array adapter to show in a ListView
-		        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-		    }
-		}
-		
-		// Create a BroadcastReceiver for ACTION_FOUND
-			mReceiver = new BroadcastReceiver() {
-		    public void onReceive(Context context, Intent intent) {
-		        String action = intent.getAction();
-		        // When discovery finds a device
-		        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-		            // Get the BluetoothDevice object from the Intent
-		            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-		            // Add the name and address to an array adapter to show in a ListView
-		            mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-		        }
-		    }
-		};
-		// Register the BroadcastReceiver
-		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-		registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-		*/
 
         Log.i("Debug", "pre_launch server");
         //Server.run();
@@ -123,19 +96,19 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         }
 
         public void run() {
-            BluetoothSocket socket = null;
+            mySocket = null;
             // Keep listening until exception occurs or a socket is returned
             while (true) {
                 try {
-                    socket = mmServerSocket.accept();
+                    mySocket = mmServerSocket.accept();
                 } catch (IOException e) {
                     Log.i("Debug", "no accept");
                     break;
                 }
                 // If a connection was accepted
-                if (socket != null) {
+                if (mySocket != null) {
                     // Do work to manage the connection (in a separate thread)
-                    manageConnectedSocket(socket);
+                    manageConnectedSocket(mySocket);
                     Log.i("Debug", "socket accept");
                     try {
                         mmServerSocket.close();
@@ -155,7 +128,64 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         }
     }
 
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        Handler mHandler;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                            .sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
     private void manageConnectedSocket(BluetoothSocket socket) {
+         connexion = new ConnectedThread(mySocket);
     }
 
     @Override
@@ -209,7 +239,7 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
             textViewToChangeGyroX.setText("GyroX = " + Float.toString(x));
             textViewToChangeGyroY.setText("GyroY = " + Float.toString(y));
             textViewToChangeGyroZ.setText("GyroZ = " + Float.toString(z));
-
+            //connexion.write();
             }
 
 	    else if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
@@ -233,6 +263,7 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 	            	textViewToChangeX.setText("X = " + Float.toString(x));
 	            	textViewToChangeY.setText("Y = " + Float.toString(y));
 	            	textViewToChangeZ.setText("Z = " + Float.toString(z));
+                    //connexion.write();
 	            }
 	            
 	            last_x = x;
